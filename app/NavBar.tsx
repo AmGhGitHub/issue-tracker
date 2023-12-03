d98892ff2f7a78568c0c8c911a49a55e4211177d
Amir <altghaderi@gmail.com>
2023-12-03 13:15:30 -0700

    Build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { FaBug } from "react-icons/fa";
+
+const NavBar = () => {
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 mb-5 border-b h-14 items-center px-5">
+      <Link href="/">
+        <FaBug />{" "}
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
