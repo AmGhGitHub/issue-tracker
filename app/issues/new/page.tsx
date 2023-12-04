e5e100676e06f394c130556d46673bcb4b604a5b
Amir <altghaderi@gmail.com>
2023-12-04 10:38:21 -0700

    refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,16 @@
 "use client";
+import ErrorMessage from "@/app/components/form-error-msg";
+import Spinner from "@/app/components/spinner";
+import { createIssueSchema } from "@/utils/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Controller, set, useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { createIssueSchema } from "@/utils/validationSchema";
-import ErrorMessage from "@/app/components/form-error-msg";
-import Spinner from "@/app/components/spinner";
-
 type issueForm = z.infer<typeof createIssueSchema>;
 
 const Page = () => {
@@ -27,6 +26,19 @@ const Page = () => {
   } = useForm<issueForm>({
     resolver: zodResolver(createIssueSchema),
   });
+
+  const onSubmit = handleSubmit(async (data: issueForm) => {
+    try {
+      setIsSubmitting(() => true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsSubmitting(() => false);
+      // set a generic error message
+      setErr("There was an error submitting the form.");
+    }
+  });
+
   return (
     <div className="max-w-xl space-y-3">
       {err && (
@@ -34,20 +46,7 @@ const Page = () => {
           <Callout.Text>{err}</Callout.Text>{" "}
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(() => true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setIsSubmitting(() => false);
-            // set a generic error message
-            setErr("There was an error submitting the form.");
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
