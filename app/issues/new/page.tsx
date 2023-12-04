e78cef3948d6060b8749f31a76d258c17c63e103
Amir <altghaderi@gmail.com>
2023-12-04 10:13:13 -0700

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,24 @@
 "use client";
-import { Button, Callout, TextField, Text } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, set, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { createIssueSchema } from "@/utils/validationSchema";
 import ErrorMessage from "@/app/components/form-error-msg";
+import Spinner from "@/app/components/spinner";
 
 type issueForm = z.infer<typeof createIssueSchema>;
 
 const Page = () => {
   const router = useRouter();
   const [err, setErr] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -35,9 +38,11 @@ const Page = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setIsSubmitting(() => true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setIsSubmitting(() => false);
             // set a generic error message
             setErr("There was an error submitting the form.");
           }
@@ -55,7 +60,9 @@ const Page = () => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
